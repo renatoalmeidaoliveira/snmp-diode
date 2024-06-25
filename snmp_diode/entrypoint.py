84dd2a9335d04c4f0f027031088efa4e526045c4
Renato Zaroubin <renato.almeida.oliveira@gmail.com>
2024-06-25 15:09:35 -0300

    entrypoint

---
+++ snmp_diode/entrypoint.py
@@ -0,0 +1,36 @@
+import argparse
+from snmp_diode import discover
+
+parser = argparse.ArgumentParser(description='SNMP Discovery Tool for NetBoxLabs Diode') 
+
+parser.add_argument('-a', '--addres', type=str, help='Target IP Address', required=False)
+parser.add_argument('-n', '--network', type=str, help='Target Network Address', required=False)
+parser.add_argument('-c', '--community', type=str, help='SNMP Community String', required=True)
+parser.add_argument('-v', '--version', type=str, help='SNMP Version', required=True)
+
+
+def main():
+    args = parser.parse_args()
+    versions = ["2", "v2c", "3"]
+    if args.addres is None and args.network is None:
+        print("Please provide either a target IP address or a target network address")
+        exit(1)
+
+    if args.version not in versions:
+        print("Please provide a valid SNMP version, options are: 2, v2c, 3")
+        exit(1)
+    
+    elif args.version == "2" or args.version == "v2c":
+        version = 2
+    elif args.version == "3":
+        version = 3
+    snmp_data = {
+        "version": version,
+        "community": args.community,
+    }
+    if args.addres:
+        device_data = discover.gater_device_data(args.addres, snmp_data)
+        print(device_data.model_dump())
+
+        
+    
